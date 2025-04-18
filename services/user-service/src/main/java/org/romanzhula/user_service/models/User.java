0aff59822d9ddb4fd16ffc8789cdd8ec1f0cdbf3
RoMANzhula <roman1985al@ukr.net>
2025-04-18 18:59:25 +0300

    Add User model - user-service

---
+++ services/user-service/src/main/java/org/romanzhula/user_service/models/User.java
@@ -0,0 +1,25 @@
+package org.romanzhula.user_service.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false) // if the user will has multiple accounts for the same email
+    private String email;
+
+}
