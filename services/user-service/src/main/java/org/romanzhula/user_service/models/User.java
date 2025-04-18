95ac153b6ede011767dcc0a2281d0f510e442ed7
RoMANzhula <roman1985al@ukr.net>
2025-04-19 00:25:05 +0300

    Up User model - user-service - add roles

--- services/user-service/src/main/java/org/romanzhula/user_service/models/User.java
+++ services/user-service/src/main/java/org/romanzhula/user_service/models/User.java
@@ -1,11 +1,16 @@
 package org.romanzhula.user_service.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.romanzhula.user_service.models.enums.UserRole;
 
 
-@Data
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
 
@@ -16,10 +21,15 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false) // if the user will has multiple accounts for the same email
     private String email;
+
+    @Enumerated(EnumType.STRING) //EnumType.ORDINAL - for numerated user roles ADMIN=0, USER=1 etc.
+    @Column(name = "role", nullable = false)
+    private UserRole role;
 
 }
