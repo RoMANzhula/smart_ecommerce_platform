5d1bde51f162ac33cf61191269ea591d0ebd2ed7
RoMANzhula <roman1985al@ukr.net>
2025-04-18 19:58:58 +0300

    Up UserService - user-service - add findByUsername() method

--- services/user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
+++ services/user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
@@ -1,14 +1,17 @@
 package org.romanzhula.user_service.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.romanzhula.user_service.controllers.requests.UserRegistrationRequest;
 import org.romanzhula.user_service.models.User;
 import org.romanzhula.user_service.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserService {
 
@@ -25,6 +28,18 @@ public class UserService {
         user.setEmail(registrationRequest.email());
 
         return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User findByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            log.error("User with username: {} NOT FOUND!", username);
+            throw  new UsernameNotFoundException("User with username: " + username + " NOT FOUND!");
+        }
+
+        return user;
     }
 
 }
