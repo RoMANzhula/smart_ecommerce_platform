789aa61ecd7efc32defb2b3f07522ce2a1e7595f
RoMANzhula <roman1985al@ukr.net>
2025-04-18 19:30:21 +0300

    Add UserRepository - user-service

---
+++ services/user-service/src/main/java/org/romanzhula/user_service/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package org.romanzhula.user_service.repositories;
+
+import org.romanzhula.user_service.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
