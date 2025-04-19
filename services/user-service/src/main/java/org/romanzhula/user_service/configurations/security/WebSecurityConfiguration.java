4d41781eb57ccf67b1b1d3a2b337b0e0aca7565f
RoMANzhula <roman1985al@ukr.net>
2025-04-19 22:42:02 +0300

    Add WebSecurityConfiguration - user-service

---
+++ services/user-service/src/main/java/org/romanzhula/user_service/configurations/security/WebSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package org.romanzhula.user_service.configurations.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/users/register",
+                                "/api/v1/users/login"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                )
+                .build()
+        ;
+    }
+
+}
