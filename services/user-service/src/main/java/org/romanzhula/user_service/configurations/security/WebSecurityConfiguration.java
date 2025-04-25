4728f5a477813e9e9ff1f65dd84adccec1a0a4c5
RoMANzhula <roman1985al@ukr.net>
2025-04-25 18:00:41 +0300

    Up WebSecurityConfiguration - user-service - change to jwt filter

--- services/user-service/src/main/java/org/romanzhula/user_service/configurations/security/WebSecurityConfiguration.java
+++ services/user-service/src/main/java/org/romanzhula/user_service/configurations/security/WebSecurityConfiguration.java
@@ -1,34 +1,51 @@
 package org.romanzhula.user_service.configurations.security;
 
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.user_service.configurations.security.jwt.AuthJWTFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfiguration {
 
+    private final AuthJWTFilter authJwtFilter;
+    private final DaoAuthenticationProvider daoAuthenticationProvider;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
                         .requestMatchers(
-                                "/api/v1/auth/register",
-                                "/api/v1/auth/login"
+                                "/api/v1/auth/**"
                         ).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .build()
+                .httpBasic(withDefaults())
+                .authenticationProvider(daoAuthenticationProvider)
+                .addFilterBefore(authJwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers((headers) ->
+                        headers
+                                .frameOptions((frameOptions) -> frameOptions.disable())
+                )
         ;
+
+        return httpSecurity.build();
     }
 
 }
