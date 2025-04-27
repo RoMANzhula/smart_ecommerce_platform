e3373f61b39f655ba0423d23f8af9d18ed9a04f7
RoMANzhula <roman1985al@ukr.net>
2025-04-27 18:50:39 +0300

    Up Inventory model - inventory-service - add Serializable

--- services/inventory-service/src/main/java/org/romanzhula/inventory_service/models/Inventory.java
+++ services/inventory-service/src/main/java/org/romanzhula/inventory_service/models/Inventory.java
@@ -4,10 +4,16 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 
 @Data
 @RedisHash("inventory")
-public class Inventory {
+public class Inventory implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     private String productId;
