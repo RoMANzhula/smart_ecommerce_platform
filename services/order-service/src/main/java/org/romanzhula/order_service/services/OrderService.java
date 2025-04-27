78ab099045831fff0d4345fcb55d1d0c7e9b195d
RoMANzhula <roman1985al@ukr.net>
2025-04-27 19:03:28 +0300

    Up OrderService - order-service - get data from inventory service by WebClient

--- services/order-service/src/main/java/org/romanzhula/order_service/services/OrderService.java
+++ services/order-service/src/main/java/org/romanzhula/order_service/services/OrderService.java
@@ -5,6 +5,7 @@ import org.romanzhula.order_service.models.Order;
 import org.romanzhula.order_service.repositories.OrderRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 @Service
@@ -13,15 +14,44 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final KafkaProducerService kafkaProducer;
+    private final WebClient webClient;
 
 
     @Transactional
     public Order createOrder(Order order) {
-        Order savedOrder = orderRepository.save(order);
+        Boolean isAvailable = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/check")
+                        .queryParam("productId", order.getProductId())
+                        .queryParam("quantity", order.getQuantity())
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block()
+        ;
 
-        kafkaProducer.publishOrderCreatedEvent(savedOrder.getId().toString());
+        if (Boolean.TRUE.equals(isAvailable)) {
+            webClient.post()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/update")
+                            .queryParam("productId", order.getProductId())
+                            .queryParam("quantityChange", -order.getQuantity())
+                            .build()
+                    )
+                    .retrieve()
+                    .bodyToMono(Void.class)
+                    .block()
+            ;
 
-        return savedOrder;
+            Order savedOrder = orderRepository.save(order);
+
+            kafkaProducer.publishOrderCreatedEvent(savedOrder.getId().toString());
+
+            return savedOrder;
+        } else {
+            throw new RuntimeException("Insufficient inventory for product: " + order.getProductId());
+        }
     }
 
 }
