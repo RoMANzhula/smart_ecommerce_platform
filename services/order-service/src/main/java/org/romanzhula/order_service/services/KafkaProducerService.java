b5de33b60b3b0b68b732a6532d6dc6f8f224e81c
RoMANzhula <roman1985al@ukr.net>
2025-04-30 22:58:26 +0300

    Up KafkaProducerService - user-service - for notification-service change publishOrderCreatedEvent

--- services/order-service/src/main/java/org/romanzhula/order_service/services/KafkaProducerService.java
+++ services/order-service/src/main/java/org/romanzhula/order_service/services/KafkaProducerService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.romanzhula.order_service.models.events.OrderCreatedEvent;
 import org.romanzhula.order_service.models.events.UserActivityEvent;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,15 @@ public class KafkaProducerService {
     private final ObjectMapper objectMapper;
 
 
-    public void publishOrderCreatedEvent(String orderId) {
-        kafkaTemplate.send("order-created-topic", orderId);
+    public void publishOrderCreatedEvent(String orderId, String userId) {
+        try {
+            OrderCreatedEvent event = new OrderCreatedEvent(orderId, userId);
+            String message = objectMapper.writeValueAsString(event);
+
+            kafkaTemplate.send("order-created-topic", message);
+        } catch (Exception e) {
+            log.error("Failed to publish Kafka event", e);
+        }
     }
 
     public void publishUserActivityViewedProductEvent(UserActivityEvent userActivityEvent) {
