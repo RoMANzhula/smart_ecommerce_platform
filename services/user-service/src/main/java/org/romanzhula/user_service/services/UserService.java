0cef8a311076b5f09398540ff01f85f8f2c22348
RoMANzhula <roman1985al@ukr.net>
2025-04-30 22:57:13 +0300

    Up UserService - user-service - for notification-service

--- services/user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
+++ services/user-service/src/main/java/org/romanzhula/user_service/services/UserService.java
@@ -3,11 +3,14 @@ package org.romanzhula.user_service.services;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.romanzhula.user_service.controllers.responses.UserResponse;
+import org.romanzhula.user_service.models.User;
 import org.romanzhula.user_service.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
+
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +30,14 @@ public class UserService {
                         Collections.singleton(user.getRole().toString())
                 ))
                 .orElseThrow(() -> new EntityNotFoundException("User not found with username: " + username))
+        ;
+    }
+
+    @Transactional(readOnly = true)
+    public String getEmailByUserId(String userId) {
+        return userRepository.findById(Long.valueOf(userId))
+                .map(User::getEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with ID: " + userId))
         ;
     }
 
